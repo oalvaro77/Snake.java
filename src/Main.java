5d16878efdf1791623e7322311e34a337f7185ef
oalvaro77 <oalvaro25@gmail.com>
2024-11-03 19:10:50 -0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        int boardWidth = 600;
+        int boardHeight = boardWidth;
+
+    }
+}
